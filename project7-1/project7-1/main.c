7e713d44ecb37801bb570603923c133f3637f7db
sowonlee0812-coder <sowon.lee0812@gmail.com>
2025-10-16 15:20:19 +0900

    Create main.c

---
+++ project7-1/project7-1/main.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char * argv[]) {
+    
+    int flag = 1;
+    int y;
+    
+    while (flag!=0)
+    {
+        y = 3;
+        flag = 0;
+    }
+    y = 4;
+
+    system("PAUSE");
+    return 0;
+}
