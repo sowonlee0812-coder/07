9b8838bcc3bcd6232c7e3772217f0a90e44e7c71
sowonlee0812-coder <sowon.lee0812@gmail.com>
2025-10-16 15:58:57 +0900

    project7-5

--- project7-1/project7-1/main.c
+++ project7-1/project7-1/main.c
@@ -1,21 +1,22 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-void f(void);
-
-int i;
+void sub(void);
 
 int main(int argc, char *argv[])
 {
-    for (i=0;i<5;i++)
-    {
-        f();
-    }
+    int i;
+    for (i=0;i<3;i++)
+        sub();
     return 0;
 }
 
-void f(void)
+void sub(void)
 {
-    for (i=0;i<10;i++)
-        printf("#");
+    int auto_count = 0;
+    static int static_count = 0;
+    auto_count++;
+    static_count++;
+    printf("auto_count = %d\n", auto_count);
+    printf("static_count = %d\n", static_count);
 }
