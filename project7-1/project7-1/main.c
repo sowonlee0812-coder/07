ed34edd83037e1f484ded15ddd45192968fbe741
sowonlee0812-coder <sowon.lee0812@gmail.com>
2025-10-16 15:31:37 +0900

    project7-3

--- project7-1/project7-1/main.c
+++ project7-1/project7-1/main.c
@@ -1,17 +1,19 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+int inc(int counter);
+
 int main(int argc, char * argv[])
 {
-    int i;
-    int temp = 1;
-
-    for (i=0;i<5;i++)
-    {
-        printf("temp = %d\n", temp);
-        temp++;
-    }
-    
-    system("PAUSE");
+    int i = 10;
+    printf("함수 호출 전 i = %d\n", i);
+    inc(i);
+    printf("함수 호출 후 i = %d\n", i);
     return 0;
+}
+
+int inc(int counter)
+{
+    counter++;
+    return counter;
 }
